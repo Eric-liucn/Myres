7b9dbfe0b991e4ef89eb1f5609dd7fdab28ffc42
Eric12324 <liushun0574@gmail.com>
2021-06-16 00:38:57 +0800

    init

---
+++ src/main/java/com/github/ericliucn/myres/Main.java
@@ -0,0 +1,29 @@
+package com.github.ericliucn.myres;
+
+import com.google.inject.Inject;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.jvm.Plugin;
+
+@Plugin(value = "myres")
+public class Main {
+
+    private final Logger logger;
+    private final PluginContainer container;
+
+    @Inject
+    public Main(final Logger logger, final PluginContainer container){
+        this.logger = logger;
+        this.container = container;
+    }
+
+    @Listener
+    public void onServerStarting(final StartingEngineEvent<Server> event){
+        this.logger.log(Level.ERROR, "This is myres!!!");
+    }
+
+}
